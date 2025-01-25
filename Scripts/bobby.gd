768e6681e67f0ae54bd05cedb223739685abb92c
Corin Varney <corinvarney@gmail.com>
2025-01-25 16:23:33 +0000

    add handling for multiple players

--- Scripts/bobby.gd
+++ Scripts/bobby.gd
@@ -1,15 +1,24 @@
 extends RigidBody3D
 
+var device
+
+func _ready():
+	device = DeviceManager.get_device()
+	print(device.color, %SoftBody3D.get_surface_override_material(0).albedo_color, %SoftBody3D.get_surface_override_material(0))
+	
+	var material = %SoftBody3D.get_surface_override_material(0).duplicate()
+	material.albedo_color = device.color
+	%SoftBody3D.set_surface_override_material(0, material)
 
 func _physics_process(delta: float) -> void:
-    const FORCE_Z = 0.05 # need to angle the force toward the sphere
-    const FORCE_MAGNITUDE = 10 # used to adjust magnitude of force
-    var radius = $CollisionShape3D.shape.get_radius();
-    var direction = Input.get_vector("P0_LEFT", "P0_RIGHT", "P0_UP", "P0_DOWN").limit_length(1)
-    var force_vector = Vector3(direction.x, -direction.y, FORCE_Z) * FORCE_MAGNITUDE
-    var force_position = Vector3(0, radius, radius)
-    if (Input.is_action_pressed("P0_UP") 
-        or Input.is_action_pressed("P0_DOWN") 
-        or Input.is_action_pressed("P0_LEFT") 
-        or Input.is_action_pressed("P0_RIGHT")):
-        self.apply_force(force_vector, force_position)
+	const FORCE_Z = 0.05 # need to angle the force toward the sphere
+	const FORCE_MAGNITUDE = 10 # used to adjust magnitude of force
+	var radius = %RigidBody3D/CollisionShape3D.shape.get_radius();
+	var direction = Input.get_vector(device.inputs.LEFT, device.inputs.RIGHT, device.inputs.UP, device.inputs.DOWN).limit_length(1)
+	var force_vector = Vector3(direction.x, -direction.y, FORCE_Z) * FORCE_MAGNITUDE
+	var force_position = Vector3(0, radius, radius)
+	if (Input.is_action_pressed(device.inputs.LEFT) 
+		or Input.is_action_pressed(device.inputs.RIGHT) 
+		or Input.is_action_pressed(device.inputs.UP) 
+		or Input.is_action_pressed(device.inputs.DOWN)):
+		%RigidBody3D.apply_force(force_vector, force_position)
